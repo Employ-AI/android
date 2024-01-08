2370db8313c4864c9d4ed8600060bc3030888298
mohsen <ash.wxrz@hotmail.com>
2024-01-08 06:10:05 +0100

    Adding modularization based on Google

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,91 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.client.build-logic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.android.tools.common)
+    compileOnly(libs.firebase.crashlytics.gradlePlugin)
+    compileOnly(libs.firebase.performance.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+    compileOnly(libs.detekt.gradlePlugin)
+    implementation(libs.truth)
+}
+
+tasks {
+    validatePlugins {
+        enableStricterValidation = true
+        failOnWarning = true
+    }
+}
+
+gradlePlugin {
+    plugins {
+        register("androidApplicationCompose") {
+            id = "employ.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+
+        register("androidApplication") {
+            id = "employ.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+
+        register("androidLibraryCompose") {
+            id = "employ.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+
+        register("androidLibrary") {
+            id = "employ.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+
+        register("androidFeature") {
+            id = "employ.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+
+        register("androidTest") {
+            id = "employ.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+
+        register("androidHilt") {
+            id = "employ.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+
+        register("androidRoom") {
+            id = "employ.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
+
+        register("jvmLibrary") {
+            id = "employ.jvm.library"
+            implementationClass = "JvmLibraryConventionPlugin"
+        }
+
+        register("detekt") {
+            id = "employ.kotlin.detekt"
+            implementationClass = "DetektConventionPlugin"
+        }
+    }
+}
