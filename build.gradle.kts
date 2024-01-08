2ddb0b582aa2e5b1333b3f91c147e060135290fb
mohsen <ash.wxrz@hotmail.com>
2024-01-08 04:17:08 +0100

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt) apply false
+}
+true
