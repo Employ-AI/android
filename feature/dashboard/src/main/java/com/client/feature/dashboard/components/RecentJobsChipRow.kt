f09256f0813b3915e49e39555334de7bcbc43e49
mohsen <ash.wxrz@hotmail.com>
2024-01-11 12:03:07 +0100

    Apply detekt

--- feature/dashboard/src/main/java/com/client/feature/dashboard/components/RecentJobsChipRow.kt
+++ feature/dashboard/src/main/java/com/client/feature/dashboard/components/RecentJobsChipRow.kt
@@ -2,7 +2,6 @@ package com.client.feature.dashboard.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
