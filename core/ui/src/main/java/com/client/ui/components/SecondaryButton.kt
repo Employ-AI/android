3b569ee2647ae22805611bbfa8b0a8ac6264b1c4
Mohsen Rezania <ash.wxrz@hotmail.com>
2024-03-14 00:52:41 +0100

    [Refactor] Refactoring Ai matching screen (#111)
    * [Refactor] Refactoring AI matching screen
    
    * [Maintenance] Removing local.properties

---
+++ core/ui/src/main/java/com/client/ui/components/SecondaryButton.kt
@@ -0,0 +1,59 @@
+package com.client.ui.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.client.ui.BaseCenterColumn
+
+@Composable
+fun SecondaryButton(
+    @StringRes text: Int,
+    onClick: () -> Unit
+) {
+    TextButton(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(54.dp),
+        onClick = onClick,
+        colors = ButtonColors(
+            containerColor = Color(0xFFEAECF0),
+            contentColor = Color(0xFF999EA9),
+            disabledContentColor = Color(0x61000000),
+            disabledContainerColor = Color(0x1F000000)
+        )
+    ) {
+        Text(
+            text = stringResource(id = text),
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
+private fun SecondaryButtonPreview() {
+    BaseCenterColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        SecondaryButton(
+            text = 0,
+            onClick = {}
+        )
+    }
+}
