4248fdd7c3e898cb0c6b8d35c5856dbc75062239
Mohsen Rzna <ash.wxrz@hotmail.com>
2024-05-01 21:23:46 +0200

    Fixes for sample tests

--- feature/account/ui/src/androidTest/java/com/client/account/ui/ExampleInstrumentedTest.kt
+++ feature/account/ui/src/androidTest/java/com/client/account/ui/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.client.account.ui.test", appContext.packageName)
+        assertEquals("com.client.employ.feature.account.ui.test", appContext.packageName)
     }
 }
--- feature/matching/ui/src/androidTest/java/com/client/feature/matching/ExampleInstrumentedTest.kt
+++ feature/matching/ui/src/androidTest/java/com/client/feature/matching/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.client.employ.feature.matching.test", appContext.packageName)
+        assertEquals("com.client.employ.feature.matching.ui.test", appContext.packageName)
     }
 }
